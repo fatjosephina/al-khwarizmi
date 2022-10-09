af7a54e8acb7b5ec0e255039479d42ad7f2f1e34
fatjosephina <55261891+fatjosephina@users.noreply.github.com>
2022-10-08 19:32:28 -0500

    Created Assignment II

---
+++ AssignmentII/AssignmentII/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AssignmentII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
